8ef566c5a5edbc31998a526d3a207b8ce95e8c32
LeeDaeMin <laalmeida@uts.edu.co>
2023-02-21 14:00:55 -0500

    add component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from '@react-three/fiber'
 import { Sky } from '@react-three/drei'
 import { Physics } from '@react-three/cannon'
+import { Ground } from '../components/Ground.jsx'
 
 function App () {
   return (
@@ -8,7 +9,7 @@ function App () {
       <Sky sunPosition={[100, 100, 20]} />
       <ambientLight intensity={0.5} />
       <Physics>
-        
+          <Ground />
       </Physics>
     </Canvas>
   )
