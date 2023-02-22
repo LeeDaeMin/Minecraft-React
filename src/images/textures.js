aa218cb76aa69625178828c23794d4cd900a9fdf
LeeDaeMin <laalmeida@uts.edu.co>
2023-02-22 02:33:28 -0500

    create others textures

--- src/images/textures.js
+++ src/images/textures.js
@@ -1,7 +1,32 @@
-import { grassImg } from "./images.js";
+import { grassImg, dirtImg, glassImg, logImg, woodImg } from "./images.js";
 
-import { TextureLoader } from "three";
+import { NearestFilter, RepeatWrapping, TextureLoader } from "three";
+
+const grassTexture = new TextureLoader().load(grassImg);
+const dirtTexture = new TextureLoader().load(dirtImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const logTexture = new TextureLoader().load(logImg);
+const woodTexture = new TextureLoader().load(woodImg);
+
 
 const groundTexture = new TextureLoader().load(grassImg);
 
-export { groundTexture }
+
+groundTexture.wrapS = RepeatWrapping;
+groundTexture.wrapT = RepeatWrapping;
+
+groundTexture.magFilter = NearestFilter;
+grassTexture.magFilter = NearestFilter;
+dirtTexture.magFilter = NearestFilter;
+glassTexture.magFilter = NearestFilter;
+logTexture.magFilter = NearestFilter;
+woodTexture.magFilter = NearestFilter;
+
+export { 
+    groundTexture,
+    grassTexture, 
+    dirtTexture, 
+    glassTexture, 
+    logTexture, 
+    woodTexture 
+}
