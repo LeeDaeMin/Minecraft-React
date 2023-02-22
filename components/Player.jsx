1da5329c2b56a815959ceaef422189d5f991750c
LeeDaeMin <laalmeida@uts.edu.co>
2023-02-22 15:22:28 -0500

    change auto move to manual move with keyboard

--- components/Player.jsx
+++ components/Player.jsx
@@ -2,8 +2,20 @@ import { useSphere } from "@react-three/cannon"
 import { useFrame, useThree } from "@react-three/fiber"
 import { useEffect, useRef } from "react"
 import { Vector3 } from "three"
+import { useKeyboard } from "../src/hooks/useKeyboard"
+
+const CHARACTER_SPEED = 4
+const CHARACTER_JUMP = 10
 
 export const Player = () => {
+    const { 
+        moveBackward,
+        moveForward,
+        moveLeft,
+        moveRight,
+        Jump 
+    } = useKeyboard()
+
     const { camera } = useThree()
     const [ref, api] = useSphere( () => ({
         mass: 1,
@@ -13,17 +25,17 @@ export const Player = () => {
 
     const pos = useRef ([0, 0, 0])
         useEffect( () => {
-            api.position.subscribe( (p) => {
+            api.position.subscribe( p => {
                 pos.current = p
             })
         }, [api.position])
     
     const vel = useRef ([0, 0, 0])
         useEffect( () => {
-            api.velocity.subscribe( (p) => {
+            api.velocity.subscribe( p => {
                 vel.current = p
             })
-        }, [api.velocity])
+        }, [api.velocity] )
     
 
     useFrame( () => {
@@ -35,7 +47,31 @@ export const Player = () => {
             )
         )
 
-        api.velocity.set( 0, 0, -1 )
+        const direction = new Vector3();
+
+        const frontVector = new Vector3(
+            0,
+            0,
+            (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+        )
+
+        const sideVector = new Vector3(
+            (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+            0,
+            0
+        )
+
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(CHARACTER_SPEED)
+            .applyEuler(camera.rotation)
+
+        api.velocity.set(
+            direction.x,
+            vel.current[1],
+            direction.z
+        )
     })
 
 
