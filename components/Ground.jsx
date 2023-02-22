ad6362a4f3a5fb3071831a8873b5992a9db53348
LeeDaeMin <laalmeida@uts.edu.co>
2023-02-22 01:22:31 -0500

    edit rotatition person adn add texture floor

--- components/Ground.jsx
+++ components/Ground.jsx
@@ -1,15 +1,17 @@
 import  { usePlane } from '@react-three/cannon'
+import { groundTexture } from '../src/images/textures.js'
+
 
 export function Ground () {
     const [ref] = usePlane(() => ({
-        rotation: [0, 0, 0],
-        position: [0, 0, 0]
+        rotation: [-Math.PI / 2, 0, 0],
+        position: [0, -0.5, 0]
     }))
 
     return (
         <mesh ref={ref}>
             <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            <meshStandardMaterial attach="material" color='green' />
+            <meshStandardMaterial attach="material" map={ groundTexture } />
         </mesh>
     )
 }
