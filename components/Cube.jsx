ca6c6810d9c011523a386ca46d4fbf48baad46e5
LeeDaeMin <laalmeida@uts.edu.co>
2023-02-27 23:03:55 -0500

    edit to cube for delete one block

--- components/Cube.jsx
+++ components/Cube.jsx
@@ -1,7 +1,9 @@
 import { useBox } from "@react-three/cannon"
+import { useState } from "react"
 import * as textures from "../src/images/textures"
 
 export const Cube = ({ position, texture }) => {
+    const [isHovered, setIsHovered] = useState(false)
     const [ref] = useBox( () => ({
         type: "Static",
         position
@@ -11,9 +13,18 @@ export const Cube = ({ position, texture }) => {
 
 
 return (
-    <mesh ref={ref}>
+    <mesh 
+    onPointerMove={ (e) => {
+        e.stopPropagation()
+        setIsHovered(true)
+    }}
+    onPointerOut={ (e) => {
+        e.stopPropagation()
+        setIsHovered(false)
+    }}
+    ref={ref}>
         <boxBufferGeometry attach='geometry' />
-        <meshStandardMaterial map={activeTexture} attach="material" />
+        <meshStandardMaterial map={activeTexture} attach="material" color={ isHovered ? 'grey' : 'white'} transparent />
     </mesh>
 )
 }
